69b09001d71b187bb0fc050c0b6edae84a9f75a1
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-04-04 23:53:44 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-supervisord
+
+set -e
+
+if [ -f /configured ]; then
+  exec /usr/bin/supervisord
+fi
+
+openssl req -new -x509 -days 365 -nodes -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN"  -out /etc/apache2/ssl/owncloud.pem -keyout /etc/apache2/ssl/owncloud.key
+
+date > /configured
+exec /usr/bin/supervisord
+
