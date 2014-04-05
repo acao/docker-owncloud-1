fa2757853766fdab39607898aa3d9ba2df6efead
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-04-04 23:54:36 -0400

    Create pre-conf.sh

---
+++ pre-conf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/usr/bin/mysqld_safe &
+ sleep 10s
+
+ mysqladmin -u root password mysqlpsswd
+ mysqladmin -u root -pmysqlpsswd reload
+ mysqladmin -u root -pmysqlpsswd create  owncloud
+
+ echo "GRANT ALL ON owncloud.* TO ownclouduser@localhost IDENTIFIED BY 'ownclouddbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
+
+ echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/community/xUbuntu_13.10/ /' >> /etc/apt/sources.list.d/owncloud.list
+ wget http://download.opensuse.org/repositories/isv:ownCloud:community/xUbuntu_13.10/Release.key
+ apt-key add - < Release.key
+ apt-get update -y -q
+ apt-get install -y -q owncloud
+
+killall mysqld
+sleep 10s
+
