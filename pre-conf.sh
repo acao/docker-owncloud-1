81a6979ef9fdd8489dc6cda095aa0480ac32bbf2
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-05-01 19:36:23 -0400

    Update pre-conf.sh
    change to ubuntu 14.04 repository for owncloud

--- pre-conf.sh
+++ pre-conf.sh
@@ -9,8 +9,8 @@
 
  echo "GRANT ALL ON owncloud.* TO ownclouduser@localhost IDENTIFIED BY 'ownclouddbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
 
- echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/community/xUbuntu_13.10/ /' >> /etc/apt/sources.list.d/owncloud.list
- wget http://download.opensuse.org/repositories/isv:ownCloud:community/xUbuntu_13.10/Release.key
+ echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/community/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud.list
+ wget http://download.opensuse.org/repositories/isv:ownCloud:community/xUbuntu_14.04/Release.key
  DEBIAN_FRONTEND=noninteractive apt-key add - < Release.key
  DEBIAN_FRONTEND=noninteractive apt-get update -y -q
  DEBIAN_FRONTEND=noninteractive apt-get install -y -q owncloud
