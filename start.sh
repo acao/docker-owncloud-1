b20547f18a76c2825bce2ea2747dfc38223a1426
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-05-04 01:07:33 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,8 @@ fi
 
 openssl req -new -x509 -days 365 -nodes -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN"  -out /etc/apache2/ssl/owncloud.pem -keyout /etc/apache2/ssl/owncloud.key
 chown -R www-data:www-data /var/www/owncloud/data
+sed -i 's/ServerName example.com/ServerName $CN/' /etc/apache2/conf.d/owncloud.conf
+
 date > /configured
 exec /usr/bin/supervisord
 
