984e5fbe19249081796ee1413ef354a5d24689b5
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-08-30 21:52:36 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,8 @@ else
         openssl req -new -x509 -days 365 -nodes -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN"  -out /etc/apache2/ssl/owncloud.pem -keyout /etc/apache2/ssl/owncloud.key
         chown -R www-data:www-data /var/www/owncloud/data
         sed -i 's/ServerName example.com/ServerName $CN/' /etc/apache2/conf.d/owncloud.conf
+        #to include the domain to the hosts
+        echo 127.0.0.1 $CN >> /etc/workaround-docker-2267/hosts
 
         date > /configured
 fi
