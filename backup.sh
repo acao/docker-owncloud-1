ccb0f072262731bf4add788590ed2e35b4e69cc4
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-26 13:00:16 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 mysqldump -u root -pmysqlpsswd --all-databases > /var/backups/alldb_backup.sql
 
 #Backup important file ... of the configuration ...
-cp  /etc/workaround-docker-2267/hosts  /var/backups/
+cp  /etc/hosts  /var/backups/
 
 #Backup importand files relate to app
 cp /var/www/owncloud/config/config.php /var/backups/
