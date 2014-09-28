41deb1d023abf1bf9dbda11f2533fa742855f2b5
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-27 23:13:43 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,15 @@
 <?php
 $CONFIG = array (
+  "installed" => false,
+  "dbtype" => "mysql",
+  "dbname" => "owncloud",
+  "dbuser" => "ownclouduser",
+  "dbpassword" => "ownclouddbpasswd",
+  "dbtableprefix" => "localhost",
   'datadirectory' => '/var/data/',
+  "forcessl" => true,
+  "blacklisted_files" => array('.htaccess'),
+  "knowledgebaseenabled" => true,
+  "appstoreenabled" => true,
+  "appstoreurl" => "https://api.owncloud.com/v1",
 );
